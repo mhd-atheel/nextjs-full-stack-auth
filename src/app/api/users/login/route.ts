79baefd7c64acd415636eb9a394d882cfff712dc
mhd-atheel <aathilmazz1234@gmail.com>
2023-08-01 17:14:26 +0530

    Login Api Compleated

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,48 @@
+import { connect } from '@/app/dbConfig/dbConfig';
+import { NextRequest, NextResponse } from 'next/server';
+import User from '@/app/models/userModel'
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken"
+
+connect();
+
+
+export async function POST(request: NextRequest) {
+
+    try {
+        const reqBody = await request.json()
+        const { username, email, password } = reqBody
+
+        const user = await User.findOne({ email })
+
+        if (user) {
+            return NextResponse.json({ error: "User already exists" }, { status: 400 })
+        }
+        const validPassword = bcryptjs.compare(password, user.password)
+        if (!validPassword) {
+            return NextResponse.json({ error: "Invalid password" }, { status: 400 })
+        }
+        console.log(user);
+
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1d" })
+
+        const response = NextResponse.json({
+            message: "Login successful",
+            success: true,
+        })
+        response.cookies.set("token", token, {
+            httpOnly: true,
+
+        })
+        return response;
+    } catch (error) {
+        return NextResponse.json({ error: error }, { status: 500 })
+    }
+    
+}
